08511f9e7dcc41d967965bcb808f8e1512a7e2d2
Epineph <heiniwinther@hotmail.com>
2024-05-16 19:00:12 +0200

    changes

--- update_repo.sh
+++ update_repo.sh
@@ -10,8 +10,8 @@ git add .
 # Commit the changes with the provided message
 git commit -m "$COMMIT_MESSAGE"
 
-# Push the changes to the master branch
-git push origin master
+# Push the changes to the main branch
+git push origin main
 
 echo "Changes committed and pushed successfully."
 
