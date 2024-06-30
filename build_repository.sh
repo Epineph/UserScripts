54b2a4ed64448bc078dd073ece25f79e0968e33e
Heini Winther Johnsen <heiniwinther@hotmail.com>
2024-07-01 01:30:08 +0200

    no comment

--- build_repository.sh
+++ build_repository.sh
@@ -7,24 +7,34 @@ Usage: build [options]
 
 Options:
   -D, --directory       Specify project directory (default: current directory).
+  -a, --auto            Use default answers for all prompts.
   -h, --help            Display this help and exit.
 
 Examples:
   build
   build -D /path/to/project
+  build -a
 EOF
 }
 
-# Function to ask user a yes/no question
+# Function to ask user a yes/no question with a default answer
 ask_yes_no() {
-    while true; do
-        read -p "$1 (y/n): " yn
-        case $yn in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
+    local question="$1"
+    local default_answer="$2"
+
+    if [ "$auto_mode" = true ]; then
+        yn=$default_answer
+    else
+        while true; do
+            read -p "$question (y/n): " yn
+            yn=${yn:-$default_answer}
+            case $yn in
+                [Yy]* ) return 0;;
+                [Nn]* ) return 1;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    fi
 }
 
 # Function to get the project path from user
@@ -39,6 +49,7 @@ get_project_path() {
 
 # Default values
 project_path=""
+auto_mode=false
 
 # Parse command-line arguments
 while [[ $# -gt 0 ]]; do
@@ -46,6 +57,10 @@ while [[ $# -gt 0 ]]; do
         -D|--directory)
             project_path="$2"
             shift 2
+            ;;
+        -a|--auto)
+            auto_mode=true
+            shift
             ;;
         -h|--help)
             show_help
@@ -61,19 +76,16 @@ done
 
 # Get project path
 if [ -z "$project_path" ]; then
-    ask_yes_no "Are you in the project directory you want to build?" || get_project_path
+    ask_yes_no "Are you in the project directory you want to build?" "y" || get_project_path
 else
     cd "$project_path" || exit 1
 fi
 
 # Ask to use or create build directory
 if [ -d "build" ]; then
-    ask_yes_no "Build directory already exists. Do you want to build from there?" || {
-        mkdir -p build
-        echo "Created build directory."
-    }
+    ask_yes_no "Build directory already exists. Do you want to build from there?" "y"
 else
-    ask_yes_no "Do you want to create a build directory?" && {
+    ask_yes_no "Do you want to create a build directory?" "y" && {
         mkdir -p build
         echo "Created build directory."
     }
@@ -82,16 +94,16 @@ fi
 # Build the project based on available files
 build_project() {
     if [ -f "CMakeLists.txt" ]; then
-        if ask_yes_no "Do you want to use cmake?"; then
+        ask_yes_no "Do you want to use cmake?" "y" && {
             cd build || exit 1
-            if ask_yes_no "Do you want to use Ninja?"; then
-                cmake -GNinja -DCMAKE_BUILD_TYPE=Release "-DCMAKE_TOOLCHAIN_FILE=/home/heini/repos/vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_INSTALL_PREFIX=$HOME/bin ..
+            ask_yes_no "Do you want to use Ninja?" "n" && {
+                cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/bin -DCMAKE_TOOLCHAIN_FILE=/home/heini/repos/vcpkg/scripts/buildsystems/vcpkg.cmake ..
                 ninja -j$(nproc)
-            else
-                cmake -DCMAKE_BUILD_TYPE=Release "-DCMAKE_TOOLCHAIN_FILE=/home/heini/repos/vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_INSTALL_PREFIX=$HOME/bin ..
+            } || {
+                cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/bin -DCMAKE_TOOLCHAIN_FILE=/home/heini/repos/vcpkg/scripts/buildsystems/vcpkg.cmake ..
                 cmake --build . --config Release -j$(nproc)
-            fi
-        fi
+            }
+        }
     elif [ -f "configure" ]; then
         ./configure --prefix=$HOME/bin
         make -j$(nproc)
@@ -142,6 +154,8 @@ install_project() {
         make install
     elif [ -f "Cargo.toml" ]; then
         cargo install --path . --root "$install_dir"
+        mv "$install_dir/bin/"* "$install_dir/"
+        rmdir "$install_dir/bin"
     elif [ -f "setup.py" ]; then
         python setup.py install --prefix="$install_dir"
     elif [ -f "pyproject.toml" ]; then
@@ -152,8 +166,12 @@ install_project() {
         elif command -v npm &> /dev/null; then
             npm install -g . --prefix "$install_dir"
         fi
+        mv "$install_dir/bin/"* "$install_dir/"
+        rmdir "$install_dir/bin"
     elif [ -f "go.mod" ]; then
         go install ./... --prefix "$install_dir"
+        mv "$install_dir/bin/"* "$install_dir/"
+        rmdir "$install_dir/bin"
     elif [ -f "Makefile.PL" ]; then
         make install PREFIX="$install_dir"
     else
@@ -166,14 +184,14 @@ install_project() {
 build_project
 
 # Ask to prepend build path to PATH variable in .zshrc
-ask_yes_no "Do you want to prepend the build path to your PATH variable in .zshrc?" && {
+ask_yes_no "Do you want to prepend the build path to your PATH variable in .zshrc?" "n" && {
     build_path=$(pwd)
     sed -i "1iexport PATH=$build_path:\$PATH" ~/.zshrc
     echo "Updated PATH in .zshrc"
 }
 
 # Ask to install the build
-ask_yes_no "Do you want to install the build?" && install_project
+ask_yes_no "Do you want to install the build?" "y" && install_project
 
 echo "Build process completed."
 
