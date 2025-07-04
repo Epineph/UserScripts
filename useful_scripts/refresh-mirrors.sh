1a3a8cef673b4421746403d783b2be5f70c52141
Epineph <heiniwinther@hotmail.com>
2025-07-04 02:51:39 +0200

    Changes made at 04-07-2025

---
+++ useful_scripts/refresh-mirrors.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+countries=(
+  Denmark
+  Germany
+  Netherlands
+  Sweden
+  Norway
+)
+
+countries_list=$(IFS=, ; echo "${countries[*]}")
+
+
+sudo reflector --verbose --country $countries_list --age 24 --lates
+t 15 --fastest 15 --sort rate --protocol https --connection-timeout
+ 5 --download-timeout 10 --cache-timeout 0 --threads 4 --save /etc/
+pacman.d/mirrorlist
